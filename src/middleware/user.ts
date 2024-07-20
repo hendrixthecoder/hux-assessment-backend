3773230c4b2ff41aa91e132a5aa4c5a93471faf3
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2024-07-20 16:05:05 +0100

    feat: login and signup endpoints validation added

---
+++ src/middleware/user.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+const userSchema = z.object({
+  firstname: z.string().min(1, "Firstname is required!"),
+  lastname: z.string().min(1, "Lastname is required!"),
+  email: z.string().email("Invalid email address").min(1, "Email is required!"),
+  password: z.string().min(6, "Password must be at least 6 characters long!"),
+  phoneNumber: z.string().min(10, "Number must be at least 10 digits long!"),
+});
+
+// Middleware to validate user creation parameters
+export const validateCreateUserParams = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validationResult = userSchema.safeParse(req.body);
+  if (!validationResult.success) {
+    return res.status(400).json({
+      message: "Invalid request!",
+      errors: validationResult.error.errors,
+    });
+  }
+  next();
+};
+
+// Middleware to validate login parameters
+export const validateLoginParams = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  const validationResult = z
+    .object({
+      email: z
+        .string()
+        .email("Invalid email address")
+        .min(1, "Email is required!"),
+      password: z
+        .string()
+        .min(6, "Password must be at least 6 characters long!"),
+    })
+    .safeParse({ email, password });
+
+  if (!validationResult.success) {
+    return res.status(400).json({
+      message: "Invalid request!",
+      errors: validationResult.error.errors,
+    });
+  }
+  next();
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/user";
+import {
+  validateCreateUserParams,
+  validateLoginParams,
+} from "../middleware/user";
 
 const router = Router();
 
-router.post("/signup", createUser);
-router.post("/login", loginUser);
+router.post("/signup", validateCreateUserParams, createUser);
+router.post("/login", validateLoginParams, loginUser);
 
 export default router;
