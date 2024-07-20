eadd276e36500cb4f7bcf37acf9b38416a4cdc40
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2024-07-20 23:14:04 +0100

    feat: finished sign up route handler

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,14 +5,31 @@ import logger from "../utils/logger";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
+    // Check if the email already exists
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    // Create a new user with the provided data
     const presaveUser = new User(req.body);
+
+    // Save the user to the database
     const user = await presaveUser.save();
 
+    // Generate a JWT token
     const token = generateToken(user._id.toString());
 
-    res.status(201).send({ token });
+    // Send the token in the response
+    res.status(201).json({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    // Send error response
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   }
 };
 
